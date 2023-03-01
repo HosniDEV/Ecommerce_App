1e1fe93170168f220abc2684a1f4ce1d7ced9075
Hosni_Cr <105177254+HosniDEV@users.noreply.github.com>
2023-03-01 14:15:31 +0100

    Ecommerce App Ecommerce App with futter and php mysql

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
-import 'modules/Account/account.dart';
-import 'modules/Cart/cart.dart';
-import 'modules/CartITems/cartItem.dart';
-import 'modules/Category/kidsClothes.dart';
-import 'modules/Category/menCategory.dart';
-import 'modules/Category/womenCategory.dart';
-import 'modules/CheckOut/checkout.dart';
-import 'modules/Discover/dicover.dart';
-import 'modules/Discover/reusableContainer.dart';
-import 'modules/LocationAndTime/location_time.dart';
-import 'modules/PaymentMethod/payment_method.dart';
-import 'modules/login/sign_up.dart';
-import 'modules/login/sign_in.dart';
-import 'package:hos_app/modules/login/forgot_password.dart';
+// import 'package:hos_app/models/ProductModel/productModel.dart';
+// import 'package:hos_app/models/provider/cartProvider.dart';
+import 'package:provider/provider.dart';
+import 'models/provider/fav_provider.dart';
+import 'models/provider/order_provider.dart';
+// import 'modules/Account/account.dart';
+// import 'modules/Cart/cart.dart';
+// import 'modules/CartITems/cartItem.dart';
+// import 'modules/Category/kidsClothes.dart';
+// import 'modules/Category/menCategory.dart';
+// import 'modules/Category/womenCategory.dart';
+// import 'modules/CheckOut/checkout.dart';
+// import 'modules/Discover/DiscoverScreen.dart';
+// import 'modules/Discover/dicover.dart';
+
+// import 'modules/LocationAndTime/location_time.dart';
+// import 'modules/PaymentMethod/payment_method.dart';
+// import 'modules/login/sign_up.dart';
+// import 'modules/login/sign_in.dart';
+// import 'package:hos_app/modules/login/forgot_password.dart';
+
 import 'modules/home/my_home_page.dart';
-import 'modules/Category/choosing_page.dart';
-import 'modules/login/update_password_page.dart';
-import 'modules/practice/first_project.dart';
-import 'layout/navigaaton_bar.dart';
-import 'modules/practice/listViewSeparated.dart';
+// import 'modules/Category/choosing_page.dart';
+// import 'modules/login/update_password_page.dart';
+
+// import 'modules/practice/cart.dart';
+// import 'modules/practice/fan_carousel_image_slider.dart';
+// import 'modules/practice/filtre.dart';
+// import 'modules/practice/first_project.dart';
+// import 'layout/navigaaton_bar.dart';
+// import 'modules/practice/listViewSeparated.dart';
+// import 'modules/practice/toggle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,9 +43,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return MaterialApp(
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => OrderProvider()),
+        ChangeNotifierProvider(create: (context) => FavProvider()),
+      ],
+      child: const MaterialApp(
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
